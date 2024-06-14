79056916f6b5261ae037a08b2ad9663a95d925a2
Anhzf <me@anhzf.dev>
2024-06-14 17:41:10 +0700

    feat: :card_file_box: Add database schema and the migrations

---
+++ db/migrations/0000_famous_jean_grey.sql
@@ -0,0 +1,87 @@
+DO $$ BEGIN
+ CREATE TYPE "public"."transaction_status" AS ENUM('pending', 'completed', 'cancelled', 'suspended');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "customers" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar NOT NULL,
+	"email" varchar,
+	"phone" varchar,
+	"address" text,
+	"type" varchar,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "customers_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "products" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar NOT NULL,
+	"merk" varchar,
+	"type" varchar,
+	"size" varchar,
+	"price" integer,
+	"is_hidden" boolean DEFAULT false NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "transaction_items" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"transaction_id" integer NOT NULL,
+	"product_id" integer NOT NULL,
+	"quantity" integer DEFAULT 1 NOT NULL,
+	"price" integer NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "transactions" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"customer_id" integer,
+	"user_id" integer NOT NULL,
+	"code" varchar,
+	"status" "transaction_status" DEFAULT 'pending' NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar NOT NULL,
+	"email" varchar NOT NULL,
+	"password" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "transaction_items" ADD CONSTRAINT "transaction_items_transaction_id_transactions_id_fk" FOREIGN KEY ("transaction_id") REFERENCES "public"."transactions"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "transaction_items" ADD CONSTRAINT "transaction_items_product_id_products_id_fk" FOREIGN KEY ("product_id") REFERENCES "public"."products"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "transactions" ADD CONSTRAINT "transactions_customer_id_customers_id_fk" FOREIGN KEY ("customer_id") REFERENCES "public"."customers"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "transactions" ADD CONSTRAINT "transactions_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "customers_email_index" ON "customers" USING btree ("email");--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "transaction_items_transaction_id_product_id_index" ON "transaction_items" USING btree ("transaction_id","product_id");--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "users_email_index" ON "users" USING btree ("email");
--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, text, timestamp, uniqueIndex, varchar } from 'drizzle-orm/pg-core';
+import { boolean, integer, pgEnum, pgTable, serial, text, timestamp, uniqueIndex, varchar } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
@@ -8,5 +8,56 @@ export const users = pgTable('users', {
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
 }, (users) => ({
-  uniqueIdx: uniqueIndex('unique_idx').on(users.email),
+  unqEmail: uniqueIndex().on(users.email),
+}));
+
+export const customers = pgTable('customers', {
+  id: serial('id').primaryKey(),
+  name: varchar('name').notNull(),
+  email: varchar('email').unique(),
+  phone: varchar('phone'),
+  address: text('address'),
+  type: varchar('type'),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+}, (customers) => ({
+  unqEmail: uniqueIndex().on(customers.email),
+}));
+
+export const products = pgTable('products', {
+  id: serial('id').primaryKey(),
+  name: varchar('name').notNull(),
+  merk: varchar('merk'),
+  type: varchar('type'),
+  size: varchar('size'),
+  price: integer('price'),
+  isHidden: boolean('is_hidden').notNull().default(false),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+});
+
+export const transactionStatusEnum = pgEnum('transaction_status', ['pending', 'completed', 'cancelled', 'suspended']);
+
+export const transactions = pgTable('transactions', {
+  id: serial('id').primaryKey(),
+  customerId: integer('customer_id').references(() => customers.id),
+  userId: integer('user_id').references(() => users.id).notNull(),
+  // Custom ID provided by the cashier if any
+  code: varchar('code'),
+  status: transactionStatusEnum('status').notNull().default('pending'),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+});
+
+export const transactionItems = pgTable('transaction_items', {
+  id: serial('id').primaryKey(),
+  transactionId: integer('transaction_id').references(() => transactions.id).notNull(),
+  productId: integer('product_id').references(() => products.id).notNull(),
+  qty: integer('quantity').notNull().default(1),
+  // Should be the price at the time of transaction
+  price: integer('price').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+}, (transactionItems) => ({
+  unqTransactionProduct: uniqueIndex().on(transactionItems.transactionId, transactionItems.productId),
 }));

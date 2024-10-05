e17c2a9f34e004edb5faa846a04160323c799875
Anhzf <me@anhzf.dev>
2024-10-05 21:45:46 +0700

    fix: :bug: Set tag list API to dynamic rendering

--- app/api/tag/route.ts
+++ app/api/tag/route.ts
@@ -1,8 +1,10 @@
 import { listTag } from '@/calls/tags';
 import { defineApi } from '@/utils/api';
+import { unstable_noStore } from 'next/cache';
 import { NextResponse } from 'next/server';
 
 export const GET = defineApi(async () => {
+  unstable_noStore();
   const tags = await listTag();
   return NextResponse.json({ data: tags });
 });
